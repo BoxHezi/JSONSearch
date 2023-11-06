07d16c3f410365ac89c3eedb7504100626b62942
boxhezi <547181293@qq.com>
2023-11-06 21:46:47 +1100

    add bootstrap, add cvss score, add link to nvd

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-var template = "<td>$cveId</td><td>$desc</td><td>$published</td>";
+var template =
+  "<td><a href='$url' target='_blank'>$cveId</a></td><td>$cvss</td><td>$desc</td><td>$published</td>";
+var resultLimit = 200; // declare a variable to limit search results
 
 function parseDesc(content) {
   return content.replace("<", "&lt;").replace(">", "&gt;").replace(/\'/g, '"');
+}
+
+function getCVSS(cve) {
+  if ("v31score" in cve) {
+    return cve.v31score;
+  } else if ("v30score" in cve) {
+    return cve.v30score;
+  } else if ("v2score" in cve) {
+    return cve.v2score;
+  }
+  return "N/A";
 }
 
 function updateDisplay(data) {
@@ -9,7 +22,9 @@ function updateDisplay(data) {
   for (const [k, v] of Object.entries(data)) {
     let tr = document.createElement("tr");
     let content = template
+      .replace("$url", v.url)
       .replace("$cveId", v.id)
+      .replace("$cvss", getCVSS(v)) // get cvss score for each cve
       .replace("$desc", parseDesc(v.descriptions[0].value))
       .replace("$published", v.published.replace("T", " "));
     tr.innerHTML = content;
@@ -18,14 +33,14 @@ function updateDisplay(data) {
   }
 }
 
-function search(value) {
-  value = value.toLowerCase();
+function doSearch(value) {
+  const toSearch = value.toLowerCase();
   let filtered = []; // list to store filtered result
   for (const [k, v] of Object.entries(window.data)) {
     if (
-      v.id.toLowerCase().includes(value) ||
-      v.descriptions[0].value.toLowerCase().includes(value) ||
-      v.published.toLowerCase().includes(value)
+      v.id.toLowerCase().includes(toSearch) ||
+      v.descriptions[0].value.toLowerCase().includes(toSearch) ||
+      v.published.toLowerCase().includes(toSearch)
     ) {
       filtered.push(window.data[k]);
     }
