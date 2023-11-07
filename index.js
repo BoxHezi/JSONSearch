0c3aca7c8eb83268b7335105fd1fd2e5c25ce032
boxhezi <547181293@qq.com>
2023-11-07 13:33:33 +1100

    add negative match; add docstring

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ function parseDesc(content) {
   return content.replace("<", "&lt;").replace(">", "&gt;").replace(/\'/g, '"');
 }
 
+/**
+ * Retrieves a description from an array of descriptions based on a specified language code.
+ *
+ * @param {Array} desc - An array of description objects. Each object should have a "lang" property specifying the language and a "value" property containing the description.
+ * @param {string} [langCode="en"] - The language code to match. Defaults to "en" (English) if not provided.
+ * @returns {string} The value of the first description that matches the provided language code, or the value of the first description in the array if no match is found.
+ */
 function getDescByLang(desc, langCode = "en") {
-  /**
-   * get description by language code
-   * if no provided language code is found, return the first description
-   */
   result = desc[0].value; // default value if no langCode is found
   for (const temp of desc) {
     if ("lang" in temp && temp["lang"] === langCode) {
@@ -48,21 +51,63 @@ function updateDisplay(data) {
   }
 }
 
-function doSearch(value) {
-  // TODO: add negative match
-  const words = value.toLowerCase().split(" ");
-  regex = "";
-  let filtered = []; // list to store filtered result
+/**
+ * Generates a regular expression based on the provided search terms.
+ *
+ * @param {Array} words - An array of search terms. If a term starts with "-", it is considered a negative match.
+ * @returns {string} A regular expression that matches any string containing all the search terms (excluding the negative matches).
+ */
+function generateRegex(words) {
+  let regex = "";
+  let negativeMatch = [];
+
   for (const w of words) {
-    regex += "(?=.*" + w + ")";
+    if (w[0] === "-") {
+      negativeMatch.push(w.substring(1));
+    } else {
+      regex += "(?=.*" + w + ")";
+    }
   }
-  for (const [k, v] of Object.entries(window.data)) {
+  if (negativeMatch.length > 0) {
+    regex += "(^((?!("; // (^((?!(n)).)*$)
+    for (let i = 0; i < negativeMatch.length; i++) {
+      regex += negativeMatch[i];
+      if (i != negativeMatch.length - 1) {
+        regex += "|";
+      }
+    }
+    regex += ")).)*$)";
+  }
+  return regex;
+}
+
+/**
+ * Filters the provided data based on the provided regular expression.
+ *
+ * @param {string} regex - A regular expression used to filter the data.
+ * @param {Object} data - The data to be filtered. Each entry should have an "id" property and a "descriptions" property. Default to window.data
+ * @returns {Array} An array containing the entries in the data that match the regular expression.
+ */
+function filterData(regex, data = window.data) {
+  let filtered = [];
+  for (const [k, v] of Object.entries(data)) {
     const testStr =
       v.id.toLowerCase() + getDescByLang(v.descriptions).toLowerCase();
     if (testStr.match(regex)) {
-      filtered.push(window.data[k]);
+      filtered.push(data[k]);
     }
   }
+  return filtered;
+}
+
+/**
+ * Performs a search operation on window.data based on the provided search terms and updates the display with the search results.
+ *
+ * @param {string} value - A string containing the search terms. If a term starts with "-", it is considered a negative match.
+ */
+function doSearch(value) {
+  const regex = generateRegex(value.toLowerCase().split(" "));
+  const filtered = filterData(regex);
   updateDisplay(filtered);
 }
 
